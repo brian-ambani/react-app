f93f6057f2330f3166cecbebfac13f056326f919
brian-ambani <brainambani1@gmail.com>
2023-08-22 04:10:55 +0300

    updated the form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from 'react'
 
 function App() {
@@ -23,11 +24,16 @@ function App() {
         reminder: false,
     }
 ])
- 
-// Delete task
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task)=> task.id !== id))
+
+// Add task
+
+const addTask = (task) =>{
+  console.log(task)
 }
+// Delete task
+  function deleteTask(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
 
 // Toggle Reminder
 const toggleReminder = (id) => {
@@ -41,6 +47,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ?(
         <Tasks tasks={tasks} onDelete=
         {deleteTask} onToggle={toggleReminder}/>
@@ -51,5 +58,4 @@ const toggleReminder = (id) => {
     </div>
   );
 }
-
 export default App;
